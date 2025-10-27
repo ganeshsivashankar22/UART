2ed33c06a38e766cf75f3d0c1feb42e15b555a7e
Ganesh s <ganeshsivashankar22@gmail.com>
2025-10-27 14:08:00 +0530

    Add files via upload

---
+++ uart6/uart config.h
@@ -0,0 +1,44 @@
+	#include<LPC21xx.h>
+
+void uart_config(void);
+
+void uart_tx(unsigned char);
+
+unsigned char uart_rx(void);
+
+
+void uart_config(void)
+
+{
+
+PINSEL0|=1<<16|1<<18;
+
+U1LCR=0x83;
+
+U1DLL=97;
+
+U1DLM=0;
+
+U1LCR=0x03;
+
+}
+
+void uart_tx(unsigned char rxbyte)
+
+{
+
+while(((U1LSR>>5)&1)==0);
+
+U1THR=rxbyte;
+
+}
+
+unsigned char uart_rx(void)
+
+{
+
+while((U1LSR&1)==0);
+
+return U1RBR;
+
+}
